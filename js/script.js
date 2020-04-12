6c64dce5331e9fc0b9f0936b5946541fe2ff506b
EnjiRouz <enji.rouz@yandex.ru>
2020-04-12 19:38:05 +0500

    Added colorful categories to table

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+/**
+ * Получение элементов по id
+ * @param id - идентификатор элемента
+ * @returns {HTMLElement}
+ */
+function getById(id){
+	return document.getElementById(id);
+}
+
+/**
+ * Получение элементов по имени класса
+ * @param className - имя класса
+ * @returns {HTMLCollectionOf<Element>}
+ */
+function getByClassName(className){
+	return document.getElementsByClassName(className);
+}
+
+/**
+ * Получение элементов по тегу
+ * @param tag - тег элемента
+ * @returns {HTMLCollectionOf<HTMLElementTagNameMap[*]>}
+ */
+function getByTag(tag){
+	return document.getElementsByTagName(tag);
+}
+
+/**
+ * Скрывает элементы заданного класса (в данном случае используется для таблицы курсов, которые можно скрыть/показать
+ * в отдельной части таблицы, меняя при этом заголовок)
+ * @param className имя класса, элементы которого требуется скрыть
+ */
+function hideElementsWithClass(className){
+	let foundElements=getByClassName(className);
+	let categoryTitle=document.getElementsByClassName("clickable "+className);
+
+	if(categoryTitle[0].textContent.includes("(показать)"))	{
+		categoryTitle[0].textContent=categoryTitle[0].textContent.replace("(показать)", "(скрыть)");
+		for(let i=0; i<foundElements.length; i++)
+			if(foundElements[i].parentElement.classList.contains("can-be-hidden"))
+				foundElements[i].classList.remove("hide");
+	}
+	else {
+		categoryTitle[0].textContent = categoryTitle[0].textContent.replace("(скрыть)", "(показать)");
+		for (let i = 0; i < foundElements.length; i++)
+			if(foundElements[i].parentElement.classList.contains("can-be-hidden"))
+				foundElements[i].classList.add("hide");
+	}
+	categoryTitle[0].classList.remove("hide");
+}
+
 // create redips container
 let redips = {};
 
